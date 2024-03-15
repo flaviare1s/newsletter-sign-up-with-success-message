0b24d26b2cdf1fb043b0e4f1b95d96a7900b1cd1
Flávia Reis <flaviarster@gmail.com>
2024-03-15 11:40:11 -0300

    Add colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Roboto': ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        tomato: 'hsl(4, 100%, 67%)',
+        dark_slate_grey: 'hsl(234, 29%, 20%)',
+        charcoal_grey: 'hsl(235, 18%, 26%)',
+        grey: 'hsl(231, 7%, 60%)',
+        white: 'hsl(0, 0%, 100%)',
+      },
+    },
   },
   plugins: [],
 }
